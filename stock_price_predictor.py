14f3b4017f9d7cf2faa7a95322750b0e7f3ec2fa
Subrat Mohanty <69408099+MohantySubrat@users.noreply.github.com>
2024-04-14 16:00:33 +0530

    Update stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -5,21 +5,30 @@ from prophet import Prophet
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
+import datetime
+from datetime import date,timedelta
 
-option = st.selectbox("Pick one", ["ONGC", "Vedanta Limited","OIL","HPCL","BPCL"])
 
-stocks = ["VEDL.NS","ONGC.NS","OIL.NS","HINDPETRO.NS","BPCL.NS"]
-stock_ONGC = yf.download(stocks[0],start="2014-01-01",end="2024-01-01")
-stock_VEDL = yf.download(stocks[1],start="2014-01-01",end="2024-01-01")
-stock_OIL = yf.download(stocks[2],start="2014-01-01",end="2024-01-01")
-stock_HINDPETRO = yf.download(stocks[3],start="2014-01-01",end="2024-01-01")
-stock_BPCL = yf.download(stocks[4],start="2014-01-01",end="2024-01-01")
+
+stocks = {"ONGC":"ONGC.NS","OIL":"OIL.NS","HPCL":"HINDPETRO.NS","BPCL":"BPCL.NS"}
+keys = list(stocks.keys())
+demo_name = st.selectbox("Choose company",keys)
+
+today = date.today()
+yesterday = today-timedelta(days=1)
+yesterday_date = yesterday.strftime("%Y-%m-%d")
+
+stock_ONGC = yf.download(stocks['ONGC'],start="2014-01-01",end=yesterday_date)
+stock_OIL = yf.download(stocks['OIL'],start="2014-01-01",end=yesterday_date)
+stock_HINDPETRO = yf.download(stocks['HPCL'],start="2014-01-01",end=yesterday_date)
+stock_BPCL = yf.download(stocks['BPCL'],start="2014-01-01",end=yesterday_date)
 
 stock_ONGC.reset_index(inplace=True)
-stock_VEDL.reset_index(inplace=True)
 stock_OIL.reset_index(inplace=True)
 stock_HINDPETRO.reset_index(inplace=True)
 stock_BPCL.reset_index(inplace=True)
+
+
 
 def forecast_func(input,col):
   input = input.rename(columns={'Date':'ds',col:'y'})
@@ -35,10 +44,6 @@ ONGC_Close = forecast_func(stock_ONGC[['Date','Close']],'Close')
 ONGC_High = forecast_func(stock_ONGC[['Date','High']],'High')
 ONGC_Low = forecast_func(stock_ONGC[['Date','Low']],'Low')
 
-VEDL_Open = forecast_func(stock_VEDL[['Date','Open']],'Open')
-VEDL_Close = forecast_func(stock_VEDL[['Date','Close']],'Close')
-VEDL_High = forecast_func(stock_VEDL[['Date','High']],'High')
-VEDL_Low = forecast_func(stock_VEDL[['Date','Low']],'Low')
 
 OIL_Open = forecast_func(stock_OIL[['Date','Open']],'Open')
 OIL_Close = forecast_func(stock_OIL[['Date','Close']],'Close')
@@ -57,8 +62,6 @@ BPCL_Low = forecast_func(stock_BPCL[['Date','Low']],'Low')
 
 forecast_table_ONGC = pd.merge((pd.merge((pd.merge(ONGC_Open,ONGC_Close,on='Date')),ONGC_Low,on='Date')),ONGC_High,on='Date')
 forecast_table_ONGC['Company'] = 'ONGC'
-forecast_table_Vedanta = pd.merge((pd.merge((pd.merge(VEDL_Open,VEDL_Close,on='Date')),VEDL_Low,on='Date')),VEDL_High,on='Date')
-forecast_table_Vedanta['Company'] = 'Vedanta Limited'
 forecast_table_OIL = pd.merge((pd.merge((pd.merge(OIL_Open,OIL_Close,on='Date')),OIL_Low,on='Date')),OIL_High,on='Date')
 forecast_table_OIL['Company'] = 'OIL'
 forecast_table_HINDPETRO = pd.merge((pd.merge((pd.merge(HINDPETRO_Open,HINDPETRO_Close,on='Date')),HINDPETRO_Low,on='Date')),HINDPETRO_High,on='Date')
@@ -66,18 +69,13 @@ forecast_table_HINDPETRO['Company'] = 'HPCL'
 forecast_table_BPCL = pd.merge((pd.merge((pd.merge(BPCL_Open,BPCL_Close,on='Date')),BPCL_Low,on='Date')),BPCL_High,on='Date')
 forecast_table_BPCL['Company'] = 'BPCL'
 
-combined_table = forecast_table_ONGC.append(forecast_table_Vedanta)
-combined_table = combined_table.append(forecast_table_OIL)
-combined_table = combined_table.append(forecast_table_HINDPETRO)
-combined_table = combined_table.append(forecast_table_BPCL)
-stock_price_data = combined_table
+combined_table = pd.concat([forecast_table_ONGC,forecast_table_OIL,forecast_table_HINDPETRO,forecast_table_BPCL])
 
-filtered_data = stock_price_data[stock_price_data['Company']==option]
 
-fig = go.Figure()
-fig.add_trace(go.Candlestick(x=filtered_data['Date'],
-              open = filtered_data['Open'], high=filtered_data['High'],
-              low=filtered_data['Low'],
-              close=filtered_data['Close']))
+selected_table=combined_table[combined_table['Company']==demo_name]
 
+fig = go.Figure((go.Candlestick(x=selected_table['Date'],
+              open = selected_table['Open'], high=selected_table['High'],
+              low=selected_table['Low'],
+              close=selected_table['Close'])))
 st.plotly_chart(fig)
