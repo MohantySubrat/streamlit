9d91fa5bd16c191e4cca73579ac5483efcf3c852
Subrat Mohanty <69408099+MohantySubrat@users.noreply.github.com>
2024-04-27 19:20:30 +0530

    Update stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -11,7 +11,7 @@ from datetime import date,timedelta
 
 
 
-stocks = {"ONGC":"ONGC.NS","OIL":"OIL.NS","HPCL":"HINDPETRO.NS","BPCL":"BPCL.NS"}
+stocks = {"ONGC":"ONGC.NS","OIL":"OIL.NS","HPCL":"HINDPETRO.NS","BPCL":"BPCL.NS","SLB"}
 keys = list(stocks.keys())
 demo_name = st.selectbox("Choose company",keys)
 
@@ -23,13 +23,14 @@ stock_ONGC = yf.download(stocks['ONGC'],start="2014-01-01",end=yesterday_date)
 stock_OIL = yf.download(stocks['OIL'],start="2014-01-01",end=yesterday_date)
 stock_HINDPETRO = yf.download(stocks['HPCL'],start="2014-01-01",end=yesterday_date)
 stock_BPCL = yf.download(stocks['BPCL'],start="2014-01-01",end=yesterday_date)
+stock_SLB = yf.download(stocks['SLB'],start="2014-01-01",end=yesterday_date)
+
 
 stock_ONGC.reset_index(inplace=True)
 stock_OIL.reset_index(inplace=True)
 stock_HINDPETRO.reset_index(inplace=True)
 stock_BPCL.reset_index(inplace=True)
-
-
+stock_SLB.reset_index(inplace=True)
 
 def forecast_func(input,col):
   input = input.rename(columns={'Date':'ds',col:'y'})
@@ -61,6 +62,12 @@ BPCL_Close = forecast_func(stock_BPCL[['Date','Close']],'Close')
 BPCL_High = forecast_func(stock_BPCL[['Date','High']],'High')
 BPCL_Low = forecast_func(stock_BPCL[['Date','Low']],'Low')
 
+SLB_Open = forecast_func(stock_BPCL[['Date','Open']],'Open')
+SLB_Close = forecast_func(stock_BPCL[['Date','Close']],'Close')
+SLB_High = forecast_func(stock_BPCL[['Date','High']],'High')
+SLB_Low = forecast_func(stock_BPCL[['Date','Low']],'Low')
+
+
 forecast_table_ONGC = pd.merge((pd.merge((pd.merge(ONGC_Open,ONGC_Close,on='Date')),ONGC_Low,on='Date')),ONGC_High,on='Date')
 forecast_table_ONGC['Company'] = 'ONGC'
 forecast_table_OIL = pd.merge((pd.merge((pd.merge(OIL_Open,OIL_Close,on='Date')),OIL_Low,on='Date')),OIL_High,on='Date')
@@ -69,8 +76,12 @@ forecast_table_HINDPETRO = pd.merge((pd.merge((pd.merge(HINDPETRO_Open,HINDPETRO
 forecast_table_HINDPETRO['Company'] = 'HPCL'
 forecast_table_BPCL = pd.merge((pd.merge((pd.merge(BPCL_Open,BPCL_Close,on='Date')),BPCL_Low,on='Date')),BPCL_High,on='Date')
 forecast_table_BPCL['Company'] = 'BPCL'
+forecast_table_SLB = pd.merge((pd.merge((pd.merge(SLB_Open,SLB_Close,on='Date')),SLB_Low,on='Date')),SLB_High,on='Date')
+forecast_table_SLB['Company'] = 'SLB'
 
-combined_table = pd.concat([forecast_table_ONGC,forecast_table_OIL,forecast_table_HINDPETRO,forecast_table_BPCL])
+
+
+combined_table = pd.concat([forecast_table_ONGC,forecast_table_OIL,forecast_table_HINDPETRO,forecast_table_BPCL,forecast_table_SLB])
 
 
 selected_table=combined_table[combined_table['Company']==demo_name]
